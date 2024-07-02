9769acf5fe52f2977deffc8465d9442e5b9e10b8
arunr <arunr@carestack.com>
2024-07-02 19:19:49 +0530

    refresh icon added

--- src/extension.ts
+++ src/extension.ts
@@ -96,6 +96,7 @@ class SidebarProvider implements vscode.WebviewViewProvider {
     private getHtmlForWebview(webview: vscode.Webview, iconsData: IconSet[]): string {
         const styleUri = webview.asWebviewUri(vscode.Uri.joinPath(this.context.extensionUri, 'media', 'style.css'));
         const scriptUri = webview.asWebviewUri(vscode.Uri.joinPath(this.context.extensionUri, 'media', 'main.js'));
+        const refreshIcon = webview.asWebviewUri(vscode.Uri.joinPath(this.context.extensionUri, 'media', 'refresh.svg'));
         const nonce = getNonce();
 
         const iconsHtml = iconsData.map((iconSet) => {
@@ -128,7 +129,7 @@ class SidebarProvider implements vscode.WebviewViewProvider {
                 <div class="input-container">
                 <input type="text" id="search" placeholder="Search icons...">
                 <div id="refresh" class="refresh-icon" title="Refresh">
-                <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 118.04 122.88"><path d="M16.08,59.26A8,8,0,0,1,0,59.26a59,59,0,0,1,97.13-45V8a8,8,0,1,1,16.08,0V33.35a8,8,0,0,1-8,8L80.82,43.62a8,8,0,1,1-1.44-15.95l8-.73A43,43,0,0,0,16.08,59.26Zm22.77,19.6a8,8,0,0,1,1.44,16l-10.08.91A42.95,42.95,0,0,0,102,63.86a8,8,0,0,1,16.08,0A59,59,0,0,1,22.3,110v4.18a8,8,0,0,1-16.08,0V89.14h0a8,8,0,0,1,7.29-8l25.31-2.3Z"/></svg>
+                <img src="${refreshIcon}" alt="Refresh">
                 </div>
                 </div>
                 
